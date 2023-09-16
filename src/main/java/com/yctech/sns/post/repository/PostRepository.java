42e83d5131240fa72a5e0c3edc7cddd7256b548a
becooq81 <becooq81@gmail.com>
2023-09-16 14:11:48 +0900

    feat: post repository

--- src/main/java/com/yctech/sns/post/repository/PostRepository.java
+++ src/main/java/com/yctech/sns/post/repository/PostRepository.java
@@ -1,2 +1,14 @@
-package com.yctech.sns.post.repository;public interface PostRepository {
+package com.yctech.sns.post.repository;
+
+import com.yctech.sns.post.domain.Post;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Serializable> {
+    List<Post> findByOrderByIdDesc();
+    List<Post> findAllByUserId(Long userId);
+    List<Post> findByIdInOrderByIdDesc(List<Long> postIdList);
 }
