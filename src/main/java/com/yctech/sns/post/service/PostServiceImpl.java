4e0abdffd59837428dc9544ecc1970b71116a301
becooq81 <becooq81@gmail.com>
2023-09-16 14:12:10 +0900

    feat: post service impl

--- src/main/java/com/yctech/sns/post/service/PostServiceImpl.java
+++ src/main/java/com/yctech/sns/post/service/PostServiceImpl.java
@@ -1,2 +1,36 @@
-package com.yctech.sns.post.service;public class PostServiceImpl {
+package com.yctech.sns.post.service;
+
+import com.yctech.sns.post.domain.Post;
+import com.yctech.sns.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Override
+    public Post addPost(String title, String content) {
+        return null;
+    }
+
+    @Override
+    public Post getPost(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Post> getPostListByUserId() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Post> getPostList() {
+        return new ArrayList<>();
+    }
 }
