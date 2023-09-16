da354ee3f761c40814e87fdbde1d0701697126ef
becooq81 <becooq81@gmail.com>
2023-09-16 14:15:00 +0900

    feat: post dto

--- src/main/java/com/yctech/sns/post/controller/dto/PostDto.java
+++ src/main/java/com/yctech/sns/post/controller/dto/PostDto.java
@@ -1,2 +1,14 @@
-package com.yctech.sns.post.controller.dto;public class PostDto {
+package com.yctech.sns.post.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PostDto(
+
+        @Schema(description = "게시물 제목", defaultValue = "디폴트 제목")
+        String title,
+
+        @Schema(description = "내용")
+        String content
+) {
+
 }
