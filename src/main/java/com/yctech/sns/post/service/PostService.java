e0f2696a975b11de89ceb2293f3693a5631005f1
becooq81 <becooq81@gmail.com>
2023-09-16 14:11:57 +0900

    feat: post service

--- src/main/java/com/yctech/sns/post/service/PostService.java
+++ src/main/java/com/yctech/sns/post/service/PostService.java
@@ -1,2 +1,15 @@
-package com.yctech.sns.post.service;public class PostService {
+package com.yctech.sns.post.service;
+
+import com.yctech.sns.post.domain.Post;
+
+import java.util.List;
+
+public interface PostService {
+    Post addPost(String title, String content);
+
+    Post getPost(Long id);
+
+    List<Post> getPostListByUserId();
+
+    List<Post> getPostList();
 }
