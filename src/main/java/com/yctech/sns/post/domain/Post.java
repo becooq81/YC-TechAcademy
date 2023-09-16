2bb61a77d86f4cb81206e950ed8653627dc97bc6
becooq81 <becooq81@gmail.com>
2023-09-16 14:11:37 +0900

    feat: post entity

--- src/main/java/com/yctech/sns/post/domain/Post.java
+++ src/main/java/com/yctech/sns/post/domain/Post.java
@@ -1,17 +1,44 @@
 package com.yctech.sns.post.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Generated;
+import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
-@Entity(name = "posts")
+@Table(name = "posts")
+@Entity
 public class Post {
-    @Id @Generated
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
 
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "content")
     private String content;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    protected Post() {}
+
+    public Post(Long userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = new Date();
+    }
+
+    public Post(Long userId, String title, String content, Date createdAt) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
 }
